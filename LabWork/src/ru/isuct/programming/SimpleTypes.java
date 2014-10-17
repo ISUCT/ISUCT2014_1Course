4ebb797958854c23a58e7adbd0ae62e9a5769c33
unknown <stud_5@5_04.efdnew.isuct.ru>
2014-10-17 11:28:32 +0400

    lab work

--- LabWork/src/ru/isuct/programming/SimpleTypes.java
+++ LabWork/src/ru/isuct/programming/SimpleTypes.java
@@ -12,7 +12,26 @@ package ru.isuct.programming;
 public class SimpleTypes {
 
     public void demo(){
-          
+ boolean aVar = true;
+ System.out.println("boolean"+aVar);
+ char bVar= 65534;
+ System.out.println("char"+bVar);
+ byte cVar= 100;
+ System.out.println("byte"+cVar);
+ short dVar= 31767;
+ System.out.println("short"+dVar);
+ int eVar= 2147483647;
+ System.out.println("int"+eVar);
+ long fVar=666;
+ System.out.println("long"+fVar);
+ float gVar= 1237;
+ System.out.println("float"+gVar);
+ double kVar=1234;
+ System.out.println("double"+kVar);
+ 
+
+ 
     }
     
 }
+
