185e357ae775c79a19e3b62cc453e694fc9fd989
unknown <stud_5@5_03.efdnew.isuct.ru>
2014-10-31 10:47:47 +0400

    4laba

--- LabWork/src/ru/isuct/programming/Launcher.java
+++ LabWork/src/ru/isuct/programming/Launcher.java
@@ -14,10 +14,18 @@ public class Launcher {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        SimpleTypes sTypes = new SimpleTypes();
-        sTypes.demo();
-        SimpleArray sArray = new SimpleArray();
-        sArray.demo();
+//        SimpleTypes sTypes = new SimpleTypes();
+//        sTypes.demo();
+//        SimpleArray sArray = new SimpleArray();
+//        sArray.demo();
         // TODO code application logic here
+        Camera myCam = new Camera();
+        myCam.sayHello();
+        Camera Panasonik = new Camera();
+ Panasonik.setName("Panasonik");
+ Panasonik.sayHello();
+ Panasonik.setPicturenumber(5);
+ int spd = Panasonik.getPicturenumber();
+ System.out.println("Picturenumber = "+ spd);
     }
 }
---
+++ LabWork/src/ru/isuct/programming/calc.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.isuct.programming;
+
+/**
+ *
+ * @author stud_5
+ */
+public class calc {
+    
+}
