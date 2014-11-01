886114b793524e33e7433649d1bf8bd7e4347bbf
Your Name <you@example.com>
2014-11-01 23:00:37 +0300

    Laba 4

--- LabWork/src/ru/isuct/programming/Launcher.java
+++ LabWork/src/ru/isuct/programming/Launcher.java
@@ -15,20 +15,20 @@ public class Launcher {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-<<<<<<< HEAD
+
       SimpleTypes eTypes = new SimpleTypes ();
       eTypes.demo();
       SimpleArray eArray = new SimpleArray ();
       eArray.demo ();
      
         // TODO code application logic here
-=======
+
       Player panasonic = new Player();
       panasonic.setName("Panasonic");
       panasonic.sayHello();
       panasonic.setTitleofsong(980665);
       int tos = panasonic.getTitleofsong();
       System.out.println("Titleofsong = "+ tos);
->>>>>>> master
+
     }
 }
