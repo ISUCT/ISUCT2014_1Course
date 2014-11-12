92e9b2be1221974a50db577c9fd0e0b4a7d37d57
demon4ik <demon4ik_lp@mail.ru>
2014-11-12 16:53:42 +0300

    1-ое тестирование

---
+++ LabWork/test/ru/isuct/programming/FortestTest.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.isuct.programming;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Администратор
+ */
+public class FortestTest {
+    
+    public FortestTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of mySumm method, of class Fortest.
+     */
+    @Test
+    public void testMySummNegative() {
+        System.out.println("mySummNegative");
+        int a = -2;
+        int b = 3;
+        Fortest instance = new Fortest();
+        int expResult = 5;
+        int result = instance.mySumm(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+    
+    @Test
+    public void testMySumm() {
+        System.out.println("mySumm");
+        int a = 2;
+        int b = 3;
+        Fortest instance = new Fortest();
+        int expResult = 5;
+        int result = instance.mySumm(a, b);
+        assertEquals(expResult, result);
+    }
+    
+}
