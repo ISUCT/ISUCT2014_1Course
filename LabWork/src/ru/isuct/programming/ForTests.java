dfdd748a4ba4fb039530a27a2169818dbb3c8892
Your Name <you@example.com>
2014-11-14 10:46:17 +0400

    testing1

---
+++ LabWork/src/ru/isuct/programming/ForTests.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.isuct.programming;
+
+/**
+ *
+ * @author Stud_5
+ */
+public class ForTests {
+      public int mySumm(int a, int b) {   
+         int c = Math.abs(a) + Math.abs(b);
+         return c;
+  }
+}   
