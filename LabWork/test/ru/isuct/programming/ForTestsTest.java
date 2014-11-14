6731c40024f9700bb92dc22f952f225310a7069f
Your Name <you@example.com>
2014-11-14 10:42:51 +0400

    TestTest

---
+++ LabWork/test/ru/isuct/programming/ForTestsTest.java
@@ -0,0 +1,76 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.isuct.programming;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Stud_5
+ */
+public class ForTestsTest {
+    
+    public ForTestsTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of mySumm method, of class ForTests.
+     */
+    @Test
+    public void testMySumm() {
+        System.out.println("mySumm");
+        int a = 7;
+        int b = 9;
+        ForTests instance = new ForTests();
+        int expResult = 16;
+        int result = instance.mySumm(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+    @Test
+    public void testMySummNegative() {
+        System.out.println("mySumm Negative");
+        int a = -5;
+        int b = 7;
+        ForTests instance = new ForTests();
+        int expResult = 12;
+        int result = instance.mySumm(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail
+}
+    @Test
+    public void testMySummBothNegative() {
+        System.out.println("mySumm Negative");
+        int a = -5;
+        int b = -7;
+        ForTests instance = new ForTests();
+        int expResult = 12;
+        int result = instance.mySumm(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail
+}
+}
