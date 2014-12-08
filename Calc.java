a983e2dca6a45c5dc5bbbe422610e5a1d72abcf9
AlexeyRovo <alexeyrovo@yandex.ru>
2014-12-08 03:29:36 +0300

    laby

---
+++ Calc.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Пакеты;
+
+/**
+ *
+ * @author Администратор
+ */
+public class Calc {
+    // y = (lg^2(a+x))/ (a+x)^2  
+    // a = 2.0 xn = 1.2 xk = 4.2 dx = 0.6 x1 = 1.16 x2 = 1.32 x3 = 1.47 x4 = 1.65 x5 = 1.93
+    
+}
