a7c4d72670f868d37e7a01baa8cab1e6297b4e35
Alexey <Alexeyrovo@yandex.ru>
2014-12-08 03:33:33 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
 	
 	public static void main(String[] args){
-		System.out.println("Nenastiev Dmitriy Aleksandrovich");
+		System.out.println("Rovo Alexey Vladimirovich");
 	}
 }
