138e13b664fa41f0076758b42f8a89b25ccc2f60
unknown <stud_5@5_15.efdnew.isuct.ru>
2014-12-12 11:27:09 +0400

    Sink Syte

--- LabWork/src/ru/isuct/programming/sinksyte/SimpleDotCom.java
+++ LabWork/src/ru/isuct/programming/sinksyte/SimpleDotCom.java
@@ -1,37 +1,47 @@
 /*
- * To change this template, choose Tools | Templates
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package ru.isuct.programming.sinksyte;
 
 /**
  *
- * @author stud_5
+ * @author stud_9
  */
 public class SimpleDotCom {
-
     int[] locationCells;
     int numOfHits = 0;
-
-    public void setLocationCells(int[] locs) {
-        locationCells = locs;
+    private int[] locationCelss;
+    
+    public void setLocationCells (int[]locs) {
+        locationCelss = locs;
     }
-
-    public String checkYorself(String stringGuess) {
-        int guess = Integer.parseInt(stringGuess);
+    
+    public String checkYourself (String stringGuess){
+        int guess = Integer.parseInt (stringGuess);
         String result = "Мимо";
-        for (int cell : locationCells) {
-            if (guess == cell) {
+        for (int cell : locationCells){
+            if (guess == cell){
                 result = "Попал";
                 numOfHits++;
                 break;
+                
+                
             }
         }
-        if (numOfHits
-                == locationCells.length) {
+        if (numOfHits == locationCells.length){
             result = "Потопил";
         }
         System.out.println(result);
-        return result;
+            return result;
+        
     }
+
+    String checkYorself(String userGuess) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
 }
