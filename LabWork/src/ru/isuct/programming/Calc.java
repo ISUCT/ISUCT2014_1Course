36e89463946792b29bc92a6c495e126a563c2d78
Your Name <you@example.com>
2014-12-12 11:29:15 +0400

    jgk

--- LabWork/src/ru/isuct/programming/Calc.java
+++ LabWork/src/ru/isuct/programming/Calc.java
@@ -1,157 +1,78 @@
-package ru.isuct.programming;
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package ru.isuct.programming;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  *
- * @author Stud_5
+ * @author user
  */
-public class Calc {
-
-    public void taskBSimple() {
-        // Вычисляем таблицу значений функции
-        //   (a+(tg)^2*b*x)
-        //y=  -------------
-        //   (B+(ctg)^2*a*x)
-        // a = 0.1
-        // b = 0.5
-        // для точек:
-        // x = 0.2
-        // x = 0.3
-         // x = 0.44
-        // x = 0.6
-        // x = 0.56
-        double a = 0.1;
-        double b = 0.5;
-        double x = 0.2;
-        double y = calcFunction(a, b, x);
-        System.out.println("x=" + x + " , y=" + y);
-
-        x = 0.3;
-        y = calcFunction(a, b, x);
-        System.out.println("x=" + x + " , y=" + y);
-
-        x = 0.44;
-        y = calcFunction(a, b, x);
-        System.out.println("x=" + x + " , y=" + y);
-
-        x = 0.6;
-        y = calcFunction(a, b, x);
-        System.out.println("x=" + x + " , y=" + y);
-
-        x = 0.56;
-        y = calcFunction(a, b, x);
-        System.out.println("x=" + x + " , y=" + y);
-
+public class CalcTest {
+    
+    public CalcTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
     }
 
-    public double calcFunction(double aLocal, double bLocal, double xLocal) {
-        double chisl = (aLocal) + Math.pow((Math.tan(bLocal * xLocal)), 2);
-        double znamen = (bLocal) + Math.pow((1 / Math.tan(aLocal * xLocal)), 2);
-        double result = chisl / znamen;
-        return result;
-        //     (sin(x)^2 + корень 3 степени a *x -b)
-        // у =  -------------------------------------
-        //              log x по основанию 5
-        // a = 2.8
-        // b = 2.5
-        // для точек: 
-        // x = 2 
-        // x = 3
-        // x = 4 
-        // x = 5
-        double a = 2.8;
-        double b = 2.5;
-        double x = 2;
-        double y = calcFunction(a, b, x);
-        System.out.println("x=" + x + " , y=" + y);
-
-        x = 3;
-        y = calcFunction(a, b, x);
-        System.out.println("x=" + x + " , y=" + y);
-
-        x = 4;
-        y = calcFunction(a, b, x);
-        System.out.println("x=" + x + " , y=" + y);
-
-        x = 5;
-        y = calcFunction(a, b, x);
-        System.out.println("x=" + x + " , y=" + y);
+    /**
+     * Test of taskBSimple method, of class Calc.
+     */
+    @Test
+    public void testTaskBSimple() {
+        System.out.println("taskBSimple");
+        Calc instance = new Calc();
+        instance.taskBSimple();
+        
     }
 
-    public void taskBImproved(double a, double b, double[] x) {
-        // Вычисляем таблицу значений функции
-        //     (sin(x)^2 + корень 3 степени a *x -b)
-        // у =  -------------------------------------
-        //              log x по основанию 5
-        // для точек передаваемых в массиве х
-        for (int i = 0; i < x.length; i+=1) {
-            double y = calcFunction(a, b, x[i]);
-            System.out.println("x=" + x[i] + " , y=" + y);
-        }
-
+    /**
+     * Test of calcFunction method, of class Calc.
+     */
+    @Test
+    public void testCalcFunction() {
+        System.out.println("calcFunction");
+        double aLocal = 0.1;
+        double bLocal = 0.5;
+        double xLocal = 0.2;
+        Calc instance = new Calc();
+        double expResult = 4.402975341621186E-5;
+        double result = instance.calcFunction(aLocal, bLocal, xLocal);
+        assertEquals(expResult, result, 0.0);
+      
     }
 
-    public void taskASimple() {
-        // Вычисляем таблицу значений функции
-        //   (a+(tg)^2*b*x)
-        //y=  -------------
-        //   (B+(ctg)^2*a*x)
-        // a = 0.1
-        // b = 0.5
-        double a = 0.1;
-        double b = 0.5;
-        double xn = 0.15;
-        double xk = 1.37;
-        double dx = 0.25;
-
-        for (double x = xn; x <= xk; x = x + dx) {// x= x + dx === x+=dx
-            double y = calcFunction(a, b, x);
-            System.out.println("x=" + x + " , y=" + y);
-        }
+    /**
+     * Test of taskASimple method, of class Calc.
+     */
+    
     }
 
-    public void taskBImproved(double a, double b, double[] x) {
-        // Вычисляем таблицу значений функции
-        //   (a+(tg)^2*b*x)
-        //y=  -------------
-        //   (B+(ctg)^2*a*x)
-        // для точек передаваемых в массиве x
-        for (int i = 0; i < x.length; i += 1) {
-            double y = calcFunction(a, b, x[i]);
-            System.out.println("x=" + x[i] + " , y=" + y);
-        }
+    /**
+     * Test of taskBImproved method, of class Calc.
+     */
+    
+    
 
-    }
-}
-        //     (sin(x)^2 + корень 3 степени a *x -b)
-        // у =  -------------------------------------
-        //              log x по основанию 5
-        // a = 2.8
-        // b = 2.5
-
-        double a = 2.8;
-        double b = 2.5;
-        double xn = 1;
-        double xk = 10;
-        double dx = 1;
-
-        for (double x = xn; x <= xk; x = x + dx) { // x = x + dx === x+=dx
-            double y = calcFunction(a, b, x);
-            System.out.println("x=" + x + " , y=" + y);
-        }
-
-    }
-
-    public double calcFunction(double aLocal, double bLocal, double xLocal) {
-        double chisl = Math.pow(Math.sin(xLocal), 2) + Math.pow((aLocal * xLocal - bLocal), (1 / 3));
-        double znamen = (Math.log(xLocal)) / (Math.log(5));
-        double result = chisl / znamen;
-        return result;
-    }
-
-}
