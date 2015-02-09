89797785c42d2ae5aa702dc92ac42b01f479542e
Your Name <you@example.com>
2015-02-09 12:48:55 +0400

    lab ten

---
+++ LabWork/src/ru/isuct/programming/SimpleGuil.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.isuct.programming;
+
+/**
+ *
+ * @author stud_17
+ */
+import javax.swing.*; 
+public class SimpleGuil {
+    public static void main (String[] args) {
+        JFrame frame = new JFrame();
+        JButton button = new JButton("click me");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(button);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+       
+        
+    }
+    
+}
