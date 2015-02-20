1d97c17ad3a3347246e0df68e8ea9a4799ab8307
Your Name <you@example.com>
2015-02-20 12:58:24 +0400

    xml

--- LabWork/src/ru/isuct/programming/Calc.java
+++ LabWork/src/ru/isuct/programming/Calc.java
@@ -1,16 +1,9 @@
-﻿/*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package ru.isuct.programming;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
@@ -21,26 +14,25 @@ public class CalcTest {
     public CalcTest() {
     }
     
-    @BeforeClass
+    
     public static void setUpClass() {
     }
     
-    @AfterClass
+    
     public static void tearDownClass() {
     }
-    
-    @Before
+   
     public void setUp() {
     }
     
-    @After
+    
     public void tearDown() {
     }
 
     /**
      * Test of taskBSimple method, of class Calc.
      */
-    @Test
+   
     public void testTaskBSimple() {
         System.out.println("taskBSimple");
         Calc instance = new Calc();
@@ -51,7 +43,7 @@ public class CalcTest {
     /**
      * Test of calcFunction method, of class Calc.
      */
-    @Test
+    
     public void testCalcFunction() {
         System.out.println("calcFunction");
         double aLocal = 0.1;
