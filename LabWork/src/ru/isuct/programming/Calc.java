a5cc4edcd3234df276ec75bdeda3c04cbb92f106
user <you@example.com>
2015-03-14 12:20:39 +0400

    Calc2

--- LabWork/src/ru/isuct/programming/Calc.java
+++ LabWork/src/ru/isuct/programming/Calc.java
@@ -1,78 +1,108 @@
-﻿/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ru.isuct.programming;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
-
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 /**
  *
- * @author user
+ * @author Stud_5
  */
-public class CalcTest {
-    
-    public CalcTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+public class Calc {
+
+    public void taskBSimple() {
+// Вычисляем таблицу значений функции 
+//    a^x-b^x
+//y=-----------*(a*b)^1/3
+//   log(a/b)по основанию 10
+// a = 0.4
+// b = 0.8
+// для точек:
+// x = 4.48
+// x = 3.56
+// x = 2.78
+// x = 5.28
+// x = 3.21
+        double a = 0.4;
+        double b = 0.8;
+        double x = 4.48;
+        double y = calcFunction(a, b, x);
+        System.out.println("x=" + x + " , y=" + y);
+        x = 3.56;
+        y = calcFunction(a, b, x);
+        System.out.println("x=" + x + " , y=" + y);
+        x = 2.78;
+        y = calcFunction(a, b, x);
+        System.out.println("x=" + x + " , y=" + y);
+        x = 5.28;
+        y = calcFunction(a, b, x);
+        System.out.println("x=" + x + " , y=" + y);
+        x = 3.21;
+        y = calcFunction(a, b, x);
+        System.out.println("x=" + x + " , y=" + y);
     }
 
-    /**
-     * Test of taskBSimple method, of class Calc.
-     */
-    @Test
-    public void testTaskBSimple() {
-        System.out.println("taskBSimple");
-        Calc instance = new Calc();
-        instance.taskBSimple();
-        
+    public double calcFunction(double a, double b, double x) {
+        double chisl = (Math.pow(a, x) - Math.pow(b, x)) * Math.pow(a * b, 1 / 3);
+        double znamen = (Math.log(a / b)) / (Math.log(10));
+        double y = chisl / znamen;
+        return y;
     }
 
-    /**
-     * Test of calcFunction method, of class Calc.
-     */
-    @Test
-    public void testCalcFunction() {
-        System.out.println("calcFunction");
-        double aLocal = 0.1;
-        double bLocal = 0.5;
-        double xLocal = 0.2;
-        Calc instance = new Calc();
-        double expResult = 4.402975341621186E-5;
-        double result = instance.calcFunction(aLocal, bLocal, xLocal);
-        assertEquals(expResult, result, 0.0);
-      
+    public void taskA(double a, double b, double xn, double xk, double xd) {
+// Вычисляем таблицу значений функции 
+//    a^x-b^x
+//y=-----------*(a*b)^1/3
+//   log(a/b)по основанию 10
+// a = 0.4
+// b = 0.8
+// для точек:
+// x = 4.48
+// x = 3.56
+// x = 2.78
+// x = 5.28
+// x = 3.21
+        for (double x = xn; x <= xk; x = x + xd) {
+            double y = calcFunction(a, b, x);
+            System.out.println("x=" + x + " , y=" + y);
+        }
     }
 
-    /**
-     * Test of taskASimple method, of class Calc.
-     */
-    
+    public double[] taskB(double a, double b, double[] x) {
+// Вычисляем таблицу значений функции 
+//    a^x-b^x
+//y=-----------*(a*b)^1/3
+//   log(a/b)по основанию 10
+// для точек передаваемых в массиве x
+        double[] y = new double[x.length];
+        for (int i = 0; i < x.length; i+=1) {
+             y[i] = calcFunction(a, b, x[i]);
+        }
+        return y;
     }
 
-    /**
-     * Test of taskBImproved method, of class Calc.
-     */
-    
-    
+    public void taskA() {
+// Вычисляем таблицу значений функции 
+//    a^x-b^x
+//y=-----------*(a*b)^1/3
+//   log(a/b)по основанию 10
+// a = 0.4
+// b = 0.8
+// для точек:
+// x = 4.48
+// x = 3.56
+// x = 2.78
+// x = 5.28
+// x = 3.21
+        double a = 0.4;
+        double b = 0.8;
+        double xn = 3.2;
+        double xk = 6.2;
+        double xd = 0.6;
+        for (double x = xn; x <= xk; x = x + xd) {
+            double y = calcFunction(a, b, x);
+            System.out.println("x=" + x + " , y=" + y);
 
+        }
+    }
+}
