51fb864f024102b108947de85b28073122594d46
Your Name <you@example.com>
2015-03-26 16:53:43 +0400

    Calk 6

--- LabWork/src/ru/isuct/programming/Calc.java
+++ LabWork/src/ru/isuct/programming/Calc.java
@@ -5,13 +5,15 @@
  */
 package ru.isuct.programming;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author user
  */
 public class Calc {
 
-    public void taskBImproved(double a,double b,double[] x) {
+    public void taskBSimple() {
         // Вычисляем таблицу значений функции
         //     (sin(x)^2 + корень 3 степени a *x -b)
         // у =  -------------------------------------
@@ -23,26 +25,82 @@ public class Calc {
         // x = 3
         // x = 4 
         // x = 5
-        for (int i = 0; i > x.length; i +=1){
-            double y = calcFunction(a ,b , x[i]);
-            System.out.println("x=" + x[i] +",y=" + y);
+        double a = 2.8;
+        double b = 2.5;
+        double x = 2;
+        double y = calcFunction(a, b, x);
+        System.out.println("x=" + x + " , y=" + y);
+
+        x = 3;
+        y = calcFunction(a, b, x);
+        System.out.println("x=" + x + " , y=" + y);
+
+        x = 4;
+        y = calcFunction(a, b, x);
+        System.out.println("x=" + x + " , y=" + y);
+
+        x = 5;
+        y = calcFunction(a, b, x);
+        System.out.println("x=" + x + " , y=" + y);
+    }
+
+    public void taskBImproved(double a, double b, double[] x) {
+        // Вычисляем таблицу значений функции
+        //     (sin(x)^2 + корень 3 степени a *x -b)
+        // у =  -------------------------------------
+        //              log x по основанию 5
+        // для точек передаваемых в массиве х
+        for (int i = 0; i < x.length; i+=1) {
+            double y = calcFunction(a, b, x[i]);
+            System.out.println("x=" + x[i] + " , y=" + y);
         }
+
     }
 
-    public double calcFunction(double a, double b, double x) {
-        double y = Math.pow((Math.log(Math.pow(a, 2) + x)), 2);
-        return y;
+    public ArrayList<Double> taskASimple (double a,double b,double xn,double xk,double dx){
+        // Вычисляем таблицу значений функции
+        //     (sin(x)^2 + корень 3 степени a *x -b)
+        // у =  -------------------------------------
+        //              log x по основанию 5
+        // a = 2.8
+        // b = 2.5
+        ArrayList <Double> y = new ArrayList <Double>() ;
+        for (double x = xn; x <= xk; x = x + dx) 
+        {
+            y.add(calcFunction(a,b,x));
+        }
+         return y;
     }
+        
+    
 
-    public void taskASimple() {
-        double a = -2.5;
-        double b = 6.3;
-        double xn = 5;
+     public void taskASimple() {
+        // Вычисляем таблицу значений функции
+        //     (sin(x)^2 + корень 3 степени a *x -b)
+        // у =  -------------------------------------
+        //              log x по основанию 5
+        // a = 2.8
+        // b = 2.5
+
+        double a = 2.8;
+        double b = 2.5;
+        double xn = 1;
         double xk = 10;
-        double dx = 0.5;
-        for (double x = xn; x <= xk; x += dx) {
-            double y = calcFunction(a, b, x);
+        double dx = 1;
+      
+        for (double x = xn; x <= xk; x = x + dx) { // x = x + dx === x+=dx
+            double y = calcFunction( a, b, x);
+            
             System.out.println("x=" + x + " , y=" + y);
         }
     }
+    
+    
+    public double calcFunction(double aLocal, double bLocal, double xLocal) {
+        double chisl = Math.pow(Math.sin(xLocal), 2) + Math.pow((aLocal * xLocal - bLocal), (1 / 3));
+        double znamen = (Math.log(xLocal)) / (Math.log(5));
+        double result = chisl / znamen;
+        return result;
+    }
+
 }
